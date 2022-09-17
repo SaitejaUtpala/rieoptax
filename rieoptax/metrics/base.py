f0500e785f64b2daa1253ff6df1dab96952a176d
Saiteja Utpala <saitejautpala@gmail.com>
2022-09-17 17:27:35 +0530

    save

--- rieoptax/metrics/base.py
+++ rieoptax/metrics/base.py
@@ -7,31 +7,8 @@ from jax import jit, vmap
 
 
 class RiemannianMetric(ABC):
-
-    def __init__(self, k):
-        self.k = k 
-        self.norm = jit(v2(self._norm))
-        self.dist = jit(v2(self._dist))
-        self.exp  = jit(v2(self._exp))
-        self.log  = jit(v2(self._log))  
-   
-    # def norm(self, tangent_vec, base_point):
-    #     return 
-        
-    # def dist(self, point_a, point_b):
-
-    # def exp(self, tangent_vec, base_point):
-    #     pass 
-
-    # def log(self, point, base_point):
-    #     pass 
-
-    # def retraction(self, tangent_vec, base_point):
-    #     pass 
-
-    # def parallel_transport(self, tangent_vec, start_point, end_point):
-    #     pass 
-
-    # def vector_transport(self, tangent_vec, start_point, end_point):
-    #     pass
+    def __init__(self):
+        j = partial(jit, static_argnums=(0,))
+        v = partial(vmap, in_axes=(0, None))
+        self.exp = jit(v(self.exp))
 
--- rieoptax/metrics/spd.py
+++ rieoptax/metrics/spd.py
@@ -1,17 +1,15 @@
 from base import RiemannianMetric
 from jax import numpy as jnp
 
-
-class SPDMetric(RiemannianMetric):
-    
+class SPDMetric(RiemannianMetric):    
     def sqrt_neg_sqrt(self, spd_matrix):
         eigval, eigvec = jnp.linalg.eigh(spd_matrix[None])
         pow_eigval = jnp.stack([jnp.power(eigval, 0.5), jnp.power(eigval, -0.5)])
         result = (pow_eigval * eigvec) @  eigvec.swapaxes(1,2)
         return result
+
+class SPDAffineInvariant(RiemannianMetric):
     
-    @partial(jit, static_argnums=(0,))
-    @partial(vmap, in_axes=(None, 0, None))
     def exp(self, tangent_vec, base_point):
         powers = self.sqrt_neg_sqrt(base_point)
         eigval, eigvec = jnp.linalg.eigh(powers[1] @ tangent_vec @ powers[1])
@@ -19,104 +17,93 @@ class SPDMetric(RiemannianMetric):
         exp = powers[0] @ middle_exp @ powers[0]
         return exp
 
+
+
+
+# @jax.tree_util.register_pytree_node_class
+# class AffineInvariant(SPDMetric):
+
+#     def __init__(self, k):
+#         self.k = k 
+
+#     def _norm(self, tangent_vec, base_point):
         
+#     def _dist(self, point_a, point_b):
 
-@jax.tree_util.register_pytree_node_class
-class SPDMetric(RiemannianMetric):
-    
-    def sqrt_neg_sqrt(spd_matrix):
-        eigval, eigvec = jnp.linalg.eigh(spd_matrix[None])
-        pow_eigval = jnp.stack([jnp.power(eigval, 0.5), jnp.power(eigval, -0.5)])
-        result = (pow_eigval * eigvec) @  eigvec.swapaxes(1,2)
-        return result
-
-
-
-
-@jax.tree_util.register_pytree_node_class
-class AffineInvariant(SPDMetric):
-
-    def __init__(self, k):
-        self.k = k 
-
-    def _norm(self, tangent_vec, base_point):
+#     def _exp(self, tangent_vec, base_point):
+#         powers = sqrt_neg_sqrt(base_point)
+#         eigval, eigvec = jnp.linalg.eigh(powers[1] @ tangent_vec @ powers[1])
+#         middle_exp = (jnp.exp(eigval).reshape(1,-1) * eigvec)@ eigvec.T
+#         exp = powers[0] @ middle_exp @ powers[0]
+#         return exp 
         
-    def _dist(self, point_a, point_b):
-
-    def _exp(self, tangent_vec, base_point):
-        powers = sqrt_neg_sqrt(base_point)
-        eigval, eigvec = jnp.linalg.eigh(powers[1] @ tangent_vec @ powers[1])
-        middle_exp = (jnp.exp(eigval).reshape(1,-1) * eigvec)@ eigvec.T
-        exp = powers[0] @ middle_exp @ powers[0]
-        return exp 
-        
-    def exp(tangent_vec, base_point):
+#     def exp(tangent_vec, base_point):
     
 
-    def _log(self, point, base_point):
-        pass 
+#     def _log(self, point, base_point):
+#         pass 
 
-    def _retraction(self, tangent_vec, base_point):
-        pass 
+#     def _retraction(self, tangent_vec, base_point):
+#         pass 
 
-    def _parallel_transport(self, tangent_vec, start_point, end_point):
-        pass 
+#     def _parallel_transport(self, tangent_vec, start_point, end_point):
+#         pass 
 
-    def _vector_transport(self, tangent_vec, start_point, end_point):
-        pass
+#     def _vector_transport(self, tangent_vec, start_point, end_point):
+#         pass
 
 
 
-@jax.tree_util.register_pytree_node_class
-class LogEuclidean(SPDMetric):
+# @jax.tree_util.register_pytree_node_class
+# class LogEuclidean(SPDMetric):
 
-    def __init__(self, k):
-        self.k = k 
+#     def __init__(self, k):
+#         self.k = k 
 
-    def _exp(self, tangent_vec, base_point):
-        pass 
+#     def _exp(self, tangent_vec, base_point):
+#         pass 
 
-    def _log(self, point, base_point):
-        pass 
+#     def _log(self, point, base_point):
+#         pass 
 
-    def _retraction(self, tangent_vec, base_point):
-        pass 
+#     def _retraction(self, tangent_vec, base_point):
+#         pass 
 
-    def _parallel_transport(self, tangent_vec, start_point, end_point):
-        pass 
+#     def _parallel_transport(self, tangent_vec, start_point, end_point):
+#         pass 
 
-    def _vector_transport(self, tangent_vec, start_poitn, end_point):
-        pass 
+#     def _vector_transport(self, tangent_vec, start_poitn, end_point):
+#         pass 
 
-@jax.tree_util.register_pytree_node_class
-class LogCholesky(SPDMetric):
-    pass
+# @jax.tree_util.register_pytree_node_class
+# class LogCholesky(SPDMetric):
+#     pass
 
-@jax.tree_util.register_pytree_node_class
-class BuresWasserstein(SPDMetric):
-    pass
+# @jax.tree_util.register_pytree_node_class
+# class BuresWasserstein(SPDMetric):
+#     pass
 
-@jax.tree_util.register_pytree_node_class
-class GeneralizedBuresWasserstein(SPDMetric):
-    pass 
+# @jax.tree_util.register_pytree_node_class
+# class GeneralizedBuresWasserstein(SPDMetric):
+#     pass 
 
-@jax.tree_util.register_pytree_node_class
-class Euclidean(SPDMetric):
+# @jax.tree_util.register_pytree_node_class
+# class Euclidean(SPDMetric):
 
-    def __init__(self, k):
-        self.k = k 
+#     def __init__(self, k):
+#         self.k = k 
 
-    def _exp(self, tangent_vec, base_point):
-        pass 
+#     def _exp(self, tangent_vec, base_point):
+#         pass 
 
-    def _log(self, point, base_point):
-        pass 
+#     def _log(self, point, base_point):
+#         pass 
 
-    def _retraction(self, tangent_vec, base_point):
-        pass 
+#     def _retraction(self, tangent_vec, base_point):
+#         pass 
 
-    def _parallel_transport(self, tangent_vec, start_point, end_point):
-        pass 
+#     def _parallel_transport(self, tangent_vec, start_point, end_point):
+#         pass 
 
-    def _vector_transport(self, tangent_vec, start_poitn, end_point):
-        pass
+#     def _vector_transport(self, tangent_vec, start_poitn, end_point):
+#         pass
