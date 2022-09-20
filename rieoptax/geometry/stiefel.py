98032485116b847e6a7953086c33a47b0cb1cc62
Saiteja Utpala <saitejautpala@gmail.com>
2022-09-20 16:31:19 +0530

    add stiefel

--- rieoptax/geometry/stiefel.py
+++ rieoptax/geometry/stiefel.py
@@ -1,22 +1,29 @@
 
-class Stiefel(RiemannianManifold):
+class StiefelManifold(RiemannianManifold):
     def __init__(self, m, r):
          if m < r or r < 1:
             raise ValueError(
-                f"Need n >= p >= 1. Values supplied were n = {n} and p = {p}"
+                f"Need m >= r >= 1. Values supplied were m = {m} and r = {r}"
             )
+        self.m = m 
+        self.r = r
 
 
-class StiefelEuclideanMetric(Stiefel):
+class StiefelEuclideanMetric(StiefelManifold):
     
     def inner_product(self, base_point, tangent_vec_a, tangent_vec_b):
-        
+        return jnp.tensordot(
+            tangent_vector_a, tangent_vector_b, axes=2
+        )
 
     def exp(self, base_point, tangent_vec):
-        pass 
+        A = base_point.T @ tangent_vec 
+        B = tangent_vec @ base_point.T
+        exp = jnp.linalg.expm(B - B.T) @ base_point @ jnp.linalg.expm(-A)
+        return exp 
 
         
-class StiefelCanonicalMetric(Stiefel):
+class StiefelCanonicalMetric(StiefelManifold):
 
 
     def inner_product(self, base_point, tangent_vec_a, tangent_vec_b):
