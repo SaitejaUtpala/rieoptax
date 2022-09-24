bec00f330e53674490fd42fdcb64a244a2cb31ff
Saiteja Utpala <saitejautpala@gmail.com>
2022-09-24 12:38:41 +0530

    save

--- rieoptax/geometry/hypersphere.py
+++ rieoptax/geometry/hypersphere.py
@@ -10,29 +10,26 @@ class HypersphereCanonicalMetric(RiemannianManifold):
 
     def exp(self, base_point, tangent_vec):
         norm = jnp.norm(tangent_vector)
-        return point * jnp.cos(norm) + tangent_vector * jnp.sinc(norm / jnp.pi)
-        
+        return point * jnp.cos(norm) + tangent_vec * jnp.sinc(norm / jnp.pi)
+
     def log(self, base_point, point):
         coeff = self.dist(base_point, point)
         v = point - base_point
-        proj = v - jnp.inner(v,x)*x
-        log = coeff * (v/jnp.norm(proj))
-        return log 
+        proj = v - jnp.inner(v, x) * x
+        log = coeff * (v / jnp.norm(proj))
+        return log
 
     def parallel_transport(self, start_point, end_point, tangent_vec):
         v = self.log(start_point, tangent_vec)
         v_norm = jnp.norm(v)
         inp = jnp.inner(v, tangent_vec)
-        a = ((jnp.cos(v_norm) -1 )*inp )/v_norm
-        b = (jnp.sin(v_norm)*inp )/v_norm
-        pt = tangent_vec  + a * tangent_vec - b * start_point
-        return pt 
-
+        a = ((jnp.cos(v_norm) - 1) * inp) / v_norm
+        b = (jnp.sin(v_norm) * inp) / v_norm
+        pt = tangent_vec + a * tangent_vec - b * start_point
+        return pt
 
     def inner_product(self, base_point, tangent_vec_a, tangent_vec_b):
-        return jnp.tensordot(
-            tangent_vector_a, tangent_vector_b, axes=1
-        )
+        return jnp.tensordot(tangent_vector_a, tangent_vector_b, axes=1)
 
     def dist(self, point_a, point_b):
         dist = jnp.clip(self.inner_product(point_a, point_a, point_b), -1, 1)
