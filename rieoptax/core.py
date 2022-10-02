7e680b306e559445445d40515400e138056ad0f0
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-02 19:57:44 +0530

    create core ManifoldArray

--- rieoptax/core.py
+++ rieoptax/core.py
@@ -1,12 +1,27 @@
 from jax import grad
+from jax.tree_util import register_pytree_node_class
 
 
-def rgrad(f):
-    rgrad = f.egrad_to_rgrad(grad(f))
-    
+@register_pytree_node_class
+class ManifoldArray:
+    """A lightweight wrapper for arrays of a model. It combines the `value`
+    (a JAX PyTree) with a corresponding manifold class.
+    """
+    def __init__(self, value, manifold=None):
+        self.value = value
+        self.manifold = manifold
 
-def geometry(manifold):
-    def wrapper(f):
-        f.egrad_to_rgrad = manifold.egrad_to_rgrad
-        return f
-    return wrapper
+    def __repr__(self):
+        return f"ManifoldParameter(value={self.value}, " \
+               f"manifold={self.manifold})" \
+
+    def tree_flatten(self):
+        children = (self.value,)
+        aux_data = self.manifold
+        return children, aux_data
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        return cls(children[0], aux_data)
+
+
