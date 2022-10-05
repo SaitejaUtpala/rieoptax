4b3c1037f3931530085d696962885e8d96cf181b
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-05 14:31:23 +0530

    fix typos

---
+++ rieoptax/optimizers/clipping.py
@@ -0,0 +1,11 @@
+from jax import numpy as jnp 
+
+def per_example_gradient_clipping(params, grads, clip_norm):
+    """Applied per-example gradient clipping."""
+    #bsize = grads[0].shape[0]
+    manifold = params.manifold
+    norms = manifold.norm(grads)
+    divisors = jnp.maximum(norms / clip_norm, 1.0)
+    num_clipped = jnp.greater(divisors, 1.0).sum()
+    clipped_sum = (jnp.moveaxis(grads, 0, -1) / divisors).sum(-1) 
+    return clipped_sum, num_clipped
