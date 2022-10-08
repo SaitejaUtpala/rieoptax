8a518509677823e8b71ae8e003c430580ac7b983
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-08 20:58:24 +0530

    add

--- rieoptax/optimizers/first_order.py
+++ rieoptax/optimizers/first_order.py
@@ -1,32 +1,16 @@
-import jax
 from combine import chain
-from core import ManifoldArray, RiemannianGradientTransformation
-from jax import numpy as jnp
 from privacy import differentially_private_aggregate
-
-
-
-
-
-def _scale_by_learning_rate(learning_rate, flip_sign=True):
-    m = -1 if flip_sign else 1
-    return scale(m * learning_rate)
+from transforms import scale_by_learning_rate
 
 
 def rsgd(learning_rate):
     """Riemannian stochastic gradient descent."""
-    return _scale_by_learning_rate(learning_rate)
+    return scale_by_learning_rate(learning_rate)
+
 
 def dp_rsgd(learning_rate, norm_clip, sigma, seed):
     "Differenitally private riemannian (stochastic) gradient descent."
     return chain(
-      differentially_private_aggregate(norm_clip=norm_clip,sigma=sigma,seed=seed),
-      _scale_by_learning_rate(learning_rate)
-  )
-    
-    
-
-
-
-
-
+        differentially_private_aggregate(norm_clip=norm_clip, sigma=sigma, seed=seed),
+        scale_by_learning_rate(learning_rate),
+    )
