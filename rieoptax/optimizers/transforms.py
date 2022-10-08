0ce34d8a8b7cc56933684626ec864d18b1a65098
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-08 20:46:32 +0530

    reformat

--- rieoptax/optimizers/transforms.py
+++ rieoptax/optimizers/transforms.py
@@ -1,7 +1,9 @@
+import jax
 import functools
 from typing import Any, Callable, NamedTuple, Optional
 
-from core import EmptyState, ManifoldArray
+from core import EmptyState, ManifoldArray, RiemannianGradientTransformation
+
 
 ScaleState = EmptyState()
 
@@ -9,12 +11,32 @@ class TraceState(NamedTuple):
   """Holds an aggregation of past updates."""
   trace: ManifoldArray 
 
-def scale(step_size):
-
-    def init_fn(params):
-        del params
 
 
+
+class EmptyState(NamedTuple):
+  """An empty state for the simplest stateless transformations."""
+
+ScaleState = EmptyState
+
+def scale(step_size) :
+  """Scale updates by some fixed scalar `step_size`."""
+
+  def init_fn(params):
+    del params 
+    return ScaleState()
+
+  def update_fn(updates, state, params=None):
+    del params
+    updates = jax.tree_util.tree_map(lambda g: step_size * g, updates)
+    return updates, state
+
+  return RiemannianGradientTransformation(init_fn, update_fn)
+
+
+def scale_by_learning_rate(learning_rate, flip_sign=True):
+    m = -1 if flip_sign else 1
+    return scale(m * learning_rate)
 
 
 
