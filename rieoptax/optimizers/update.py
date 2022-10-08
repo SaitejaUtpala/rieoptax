079cac00ff50f0a7ec7d63e44d3093d1baae7858
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-08 20:40:00 +0530

    save

--- rieoptax/optimizers/update.py
+++ rieoptax/optimizers/update.py
@@ -2,9 +2,8 @@ import jax
 from core import ManifoldArray
 
 
+
 def apply_update(params, updates):
     """Applies an update to the corresponding parameters by using
     Riemannian Exponential Map and returns updated parameters on manifold."""
-    return jax.tree_map(
-        lambda p, u: ManifoldArray(p.manifold.exp(u), p.manifold), params, updates
-    )
+    return ManifoldArray(params.manifold.retr(params.value,updates), params.manifold)
