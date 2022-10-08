2fdfc3657222afc677c60df0998ff27bc0c07ce3
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-08 21:00:10 +0530

    add rasa

--- rieoptax/optimizers/first_order.py
+++ rieoptax/optimizers/first_order.py
@@ -1,12 +1,19 @@
 from combine import chain
 from privacy import differentially_private_aggregate
-from transforms import scale_by_learning_rate
+from transforms import scale_by_learning_rate, variance_reduction
 
 
 def rsgd(learning_rate):
     """Riemannian stochastic gradient descent."""
     return scale_by_learning_rate(learning_rate)
 
+def rasa(learning_rate, beta):
+  """Riemannian adaptive stochastic algorithm."""
+  return chain(variance_reduction(), scale_by_learning_rate(learning_rate))
+
+def rsvrg(learning_rate):
+    """Riemannain stochastic variance reduction gradient descent."""
+    return chain(variance_reduction(), scale_by_learning_rate(learning_rate))
 
 def dp_rsgd(learning_rate, norm_clip, sigma, seed):
     "Differenitally private riemannian (stochastic) gradient descent."
