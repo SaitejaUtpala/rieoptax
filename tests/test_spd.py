505c9acacfc67eeba84a1c5d2db2bb573eb7503d
Saiteja Utpala <73220310+SaitejaUtpala@users.noreply.github.com>
2022-10-15 14:17:36 +0000

    add SPDAIEXP

--- tests/test_spd.py
+++ tests/test_spd.py
@@ -1,17 +1,17 @@
 
 from absl.testing import absltest
-import chex
+from chex import assert_trees_all_close
 from rieoptax.geometry.spd import SPDAffineInvariant
 class TestSPDAffineInvarinat(absltest.TestCase):
     
     manifold = SPDAffineInvariant(2,2)
     def test_exp():
-        bp = jnp.array()
-        tv = jnp.array()
-        output = manifold.exp(bp, tv)
-        expected = 
-        chex.assert_trees_all_close(outptu, expected)
-            
+        manifold = SPDAffineInvariant()
+        tv =jnp.array([[2.0, 0.0], [0.0, 2.0]])
+        bp =jnp.array([[1.0, 0.0], [0.0, 1.0]])
+        expected=jnp.array([[jnp.exp(2), 0.0], [0.0, jnp.exp(2)]])
+        assert_trees_all_close(manifold.exp(bp, tv), expected)
+
     def test_log():
         
     def test_pt():
