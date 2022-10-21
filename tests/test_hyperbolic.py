feffd98c358ed8a24ebbc940d2ce1ec52ecdcc19
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-21 15:51:15 +0530

    remove poincare tests for now

--- tests/test_hyperbolic.py
+++ tests/test_hyperbolic.py
@@ -9,14 +9,4 @@ from rieoptax.geometry.hyperbolic import PoincareBall
 class TestPoincareBall(absltest.TestCase):
     manifold = PoincareBall(2)
 
-    def test_mobius_add(self):
-        pt_a = jnp.array([])
-        pt_b = jnp.array([])
-        exp = jnp.array([])
-        assert_trees_all_close(self.manifold.mobius_add(pt_a, pt_b), exp)
-
-    def test_mobius_sub(self):
-        pt_a = jnp.array([])
-        pt_b = jnp.array([])
-        exp = jnp.array([])
-        assert_trees_all_close(self.manifold.mobius_sub(pt_a, pt_b), exp)
+   
