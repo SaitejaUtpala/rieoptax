e70ec579177483dba7c80f0518485e47e2036e4b
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-26 20:43:45 +0530

    first version of retraction of Coupling manifold

--- rieoptax/geometry/coupling.py
+++ rieoptax/geometry/coupling.py
@@ -8,13 +8,13 @@ from rieoptax.geometry.base import RiemannianManifold
 
 class CouplingManifold(RiemannianManifold):
     def __init__(self, p, q):
-        self.p = p 
-        self.q = q 
+        self.p = p
+        self.q = q
         self.n = p.shape[0]
         self.m = q.shape[0]
-        self.dim = (self.n-1) * (self.m-1)
+        self.dim = (self.n - 1) * (self.m - 1)
 
-    def ext_sinkhorn_knopp(self, pmat : Array, iter : int) -> Tuple[Array, Array] :
+    def ext_sinkhorn_knopp(self, pmat: Array, iter: int) -> Tuple[Array, Array]:
         """Extended Sinkhorn Knopp Algorithm.
 
         Args:
@@ -22,20 +22,20 @@ class CouplingManifold(RiemannianManifold):
             iter: number of iterations.
 
         Returns:
-            returns tuple of Diagonal Scaling matrices .
+            returns tuple of Diagonal Scaling matrices.
         """
         ones = jnp.ones(self.m)
         pmat_T = pmat.T
-        d1 = self.q/(pmat @ ones) 
-        d2 = self.p/(pmat_T @ d1 )
+        d1 = self.q / (pmat @ ones)
+        d2 = self.p / (pmat_T @ d1)
         for _ in range(iter):
-            d1 = self.q/(pmat @ d2) 
-            d2 = self.p/(pmat_T @ d1 )
+            d1 = self.q / (pmat @ d2)
+            d2 = self.p / (pmat_T @ d1)
         return d1, d2
 
-    def ld_ext_sinkhorn_knopp(self, pmat : Array , iter : int) -> Array :
+    def ld_ext_sinkhorn_knopp(self, pmat: Array, iter: int) -> Array:
         """Extended Sinkhorn Knopp Algorithm operated in Log Domain.
-        Note : log domain extended sinkhorn knopp algorithm is more 
+        Note : log domain extended sinkhorn knopp algorithm is more
         numerically stable thant 'ext_sinkhorn_knopp'.
 
         Args:
@@ -45,47 +45,55 @@ class CouplingManifold(RiemannianManifold):
         Returns:
             returns tuple of Diagonal Scaling matrices .
         """
-        pass 
+        pass
 
 
 class CouplingFisher(CouplingManifold):
-    
-    def inp(self, bpt : Array, tv_a : Array, tv_b : Array) -> float:
-        mat = (tv_a * tv_b)/bpt
+    def inp(self, bpt: Array, tv_a: Array, tv_b: Array) -> float:
+        mat = (tv_a * tv_b) / bpt
         return mat.sum()
 
-    def ts_proj(self, bpt : Array, vec: Array) -> Array :
+    def ts_proj(self, bpt: Array, vec: Array) -> Array:
         """Orthogonal projection on tangent space.
 
         Args:
             bpt: base_point.
-            pt: tangent_vec.
+            vec: vector in ambient space, a Positive matrix.
 
         Returns:
             returns orthogonal projection of 'vec' onto T_{bpt}M .
         """
-        pass  
+        pass
 
-        
-        
-    def retr(self, bp, tv):
-        pass 
+    def retr(self, bpt : Array, tv : Array, iter : int) -> Array:
+        """Retraction mapping.
+
+        Args:
+            bpt: base_point.
+            tv: tangent_vec.
+            iter: number of iterations for sinkhorn knop. 
+
+        Returns:
+            returns .
+        """
+        return self.ld_ext_sinkhorn_knopp( bpt * jnp.exp(tv/bpt), 10)
 
 
 class Multinomial(CouplingManifold):
     def __init__(self, p):
-        self.p = p 
+        self.p = p
+
 
 class SPDMultinomial(CouplingManifold):
     def __init__(self, p):
         self.p = p
 
+
 class SYMMultinomial(CouplingManifold):
     def __init__(self, p):
         self.p = p
 
+
 class DSMultinomial(CouplingManifold):
     def __init__(self, p):
         self.p = p
-
-
