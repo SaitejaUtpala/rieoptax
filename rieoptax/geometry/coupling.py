cb73d484d4677f1934d23d3d87b27f16d08155c3
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-26 18:55:29 +0530

    add template for extended sinkhorn knopp

--- rieoptax/geometry/coupling.py
+++ rieoptax/geometry/coupling.py
@@ -1,3 +1,5 @@
+from chex import Array
+
 from rieoptax.geometry.base import RiemannianManifold
 
 
@@ -9,12 +11,30 @@ class CouplingManifold(RiemannianManifold):
         self.m = q.shape[0]
         self.dim = (self.n-1) * (self.m-1)
 
+    def extended_sinkhorn_knopp():
+        pass 
+
 
 class CouplingFisher(CouplingManifold):
     
-    def inp(self, bp, tv_a, tv_b):
-        pass 
+    def inp(self, bpt : Array, tv_a : Array, tv_b : Array) -> float:
+        mat = (tv_a * tv_b)/bpt
+        return mat.sum()
 
+    def ts_proj(self, bpt : Array, vec: Array) -> Array :
+        """Orthogonal projection on tangent space.
+
+        Args:
+            bpt: base_point.
+            pt: tangent_vec.
+
+        Returns:
+            returns orthogonal projection of 'vec' onto T_{bpt}M .
+        """
+        pass  
+
+        
+        
     def retr(self, bp, tv):
         pass 
 
@@ -23,6 +43,16 @@ class Multinomial(CouplingManifold):
     def __init__(self, p):
         self.p = p 
 
+class SPDMultinomial(CouplingManifold):
+    def __init__(self, p):
+        self.p = p
+
+class SYMMultinomial(CouplingManifold):
+    def __init__(self, p):
+        self.p = p
+
 class DSMultinomial(CouplingManifold):
     def __init__(self, p):
         self.p = p
+
+
