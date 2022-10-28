for numerical stability fixes

rieoptax/nn/hyperbolic.py
@@ -114,9 +114,11 @@ class PoincareRNNCell(nn.Module):
 
     Attributes:
         curv: curvature of the poincare manifold.
-        gate_fn: activation function used for gates (default: sigmoid)
+        eps: eps to be added inputs, h while doing operations, done
+            to increase numerical stability of operations (default: 1E-15).
+        gate_fn: activation function used for gates (default: sigmoid).
         activation_fn: activation function used for output and memory update
-        (default: tanh).
+            (default: tanh).
         kernel_init: initializer function for the kernels that transform
             the input (default: lecun_normal).
         recurrent_kernel_init: initializer function for the kernels that transform
@@ -125,6 +127,7 @@ class PoincareRNNCell(nn.Module):
     """
 
     curv: float = -1.0
+    eps: float = 1e-15
     gate_fn: Callable[..., Any] = sigmoid
     activation_fn: Callable[..., Any] = tanh
     kernel_init: Callable = default_kernel_init
@@ -146,7 +149,8 @@ class PoincareRNNCell(nn.Module):
         Returns:
         A tuple with the new carry and the output.
         """
-        h = carry
+        h = carry + self.eps
+        inputs = inputs + self.eps
         hidden_features = h.shape[-1]
         manifold = PoincareBall(hidden_features, self.curv)
         mobius_add = vmap(manifold.mobius_add, in_axes=(0, 0))
@@ -189,19 +193,19 @@ class PoincareGRUCell(nn.Module):
     Attributes:
         curv: curvature of the poincare manifold (default: -1).
         eps: eps to be added inputs, h while doing operations, done
-            to increase numerical stability of operations (default: 1E-15). 
-        gate_fn: activation function used for gates (default: sigmoid)
+            to increase numerical stability of operations (default: 1E-15).
+        gate_fn: activation function used for gates (default: sigmoid).
         activation_fn: activation function used for output and memory update
             (default: tanh).
         kernel_init: initializer function for the kernels that transform
             the input (default: lecun_normal).
         recurrent_kernel_init: initializer function for the kernels that transform
             the hidden state (default: orthogonal).
-        bias_init: initializer for the bias parameters (default: zeros)
+        bias_init: initializer for the bias parameters (default: zeros).
     """
 
     curv: float = -1.0
-    eps : float = 1e-15
+    eps: float = 1e-15
     gate_fn: Callable[..., Any] = sigmoid
     activation_fn: Callable[..., Any] = tanh
     kernel_init: Callable = default_kernel_init
@@ -222,7 +226,7 @@ class PoincareGRUCell(nn.Module):
         Returns:
             A tuple with the new carry and the output.
         """
-        h = carry + self.eps 
+        h = carry + self.eps
         inputs = inputs + self.eps
         hidden_features = h.shape[-1]
         manifold = PoincareBall(hidden_features, self.curv)