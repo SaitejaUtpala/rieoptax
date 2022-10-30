cfe2eab85a2e50ccb987fc21df1c42ba2182a90b
Saiteja Utpala <saitejautpala@gmail.com>
2022-10-30 22:14:26 +0530

    complete testing main parts

--- tests/test_hyperbolic.py
+++ tests/test_hyperbolic.py
@@ -1,4 +1,3 @@
-
 from absl.testing import absltest
 from chex import assert_trees_all_close
 from jax import numpy as jnp
@@ -9,28 +8,29 @@ from rieoptax.geometry.hyperbolic import PoincareBall
 class TestPoincareBall(absltest.TestCase):
     manifold = PoincareBall(2)
     sqrt2 = jnp.sqrt(2)
-    bpt = jnp.array([0.1/sqrt2, 0.1/sqrt2])
+    bpt = jnp.array([0.1 / sqrt2, 0.1 / sqrt2])
     tv = jnp.array([1.0, 2.0])
-    pt = jnp.array([0.2/sqrt2, 0.2/sqrt2])
-    
-   
+    pt = jnp.array([0.2 / sqrt2, 0.2 / sqrt2])
+
     def test_mobius_add(self):
         exptd = jnp.array([0.2079726, 0.2079726])
-        assert_trees_all_close(self.manifold.mobius_add(self.bpt,self.pt), exptd)
+        assert_trees_all_close(self.manifold.mobius_add(self.bpt, self.pt), exptd)
 
     def test_dist(self):
         exptd = jnp.array(0.20479442)
         assert_trees_all_close(self.manifold.dist(self.bpt, self.pt), exptd)
 
     def test_ptrans(self):
-        exptd = jnp.array([0.96969694, 1.9393936 ])
+        exptd = jnp.array([0.96969694, 1.9393936])
         assert_trees_all_close(self.manifold.ptrans(self.bpt, self.pt, self.tv), exptd)
 
     def test_sdist_to_gyroplanes(self):
         exptd = jnp.array(0.19441889)
-        assert_trees_all_close(self.manifold.sdist_to_gyroplanes(self.bpt, self.tv, self.pt), exptd)
+        assert_trees_all_close(
+            self.manifold.sdist_to_gyroplanes(self.bpt, self.tv, self.pt), exptd
+        )
 
-
-
-
-   
+    def test_mobius_matvec(self):
+        exptd = jnp.array([0.19429359, 0.45335174])
+        M = jnp.array([[1.0, 2.0], [3.0, 4.0]])
+        assert_trees_all_close(self.manifold.mobius_matvec(M, self.bpt), exptd)
