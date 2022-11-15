8c051f0dbc63a604c7d9fcde8cf2a0fffd842bb4
Saiteja Utpala <saitejautpala@gmail.com>
2022-11-15 16:27:39 +0530

    new core Autograd donw

--- rieoptax/core.py
+++ rieoptax/core.py
@@ -122,5 +122,4 @@ def straight_through_f(f: Callable) -> Callable:
     def _f(x: Array) -> Array:
         zero = x - lax.stop_gradient(x)
         return zero + lax.stop_gradient(f(x))
-
     return _f
