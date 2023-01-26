2ae5ad9e5a4713d5d0357a29173169fdf37bf134
Saiteja Utpala <saitejautpala@gmail.com>
2023-01-26 18:33:19 +0530

    [MAJOR-UPDATE] Remove ManifoldArray and add ManifoldObj is constructed.

---
+++ rieoptax/optimizers/base.py
@@ -0,0 +1,71 @@
+import importlib
+from chex import Array
+from typing import Callable, NamedTuple, Any, Dict, List
+from typing_extensions import Protocol
+
+from flax.core import FrozenDict
+from flax.traverse_util import (_get_params_dict, _sorted_items, empty_node,
+                                flatten_dict, unflatten_dict)
+from rieoptax.geometry.euclidean import Euclidean
+
+
+def construct_manifold_obj(param_name: str):
+    if "@" not in param_name:
+        return Euclidean()
+    first_split = param_name.split("@")
+    module_name = "rieoptax.geometry." + first_split[1]
+    mo = first_split[2].split("(")
+    manifold_name = mo[0]
+    manifold_params = mo[1][:-1].split(",")
+    return obj_from_str(module_name, manifold_name, manifold_params)
+
+
+def obj_from_str(module_name: str, class_name: str, str_params: List[str]):
+
+    m = importlib.import_module(module_name)
+    c = getattr(m, class_name)
+    obj = c.from_str(*str_params)
+    return obj
+
+
+def get_manifold_dict(inputs: Dict[str, Any]) -> Dict[str, Any]:
+    params = _get_params_dict(inputs)
+    flat_dict = flatten_dict(params, keep_empty_nodes=True)
+    new_dict = {}
+    for key, value in _sorted_items(flat_dict):
+        if value is not empty_node:
+            path = "/" + "/".join(key)
+            name = key[-1]
+            new_dict[key] = construct_manifold_obj(name)
+    new_params = unflatten_dict(new_dict)
+
+    if isinstance(inputs, flax.core.FrozenDict):
+        return FrozenDict(new_params)
+    else:
+        return new_params
+
+
+class TransformInitFn(Protocol):
+    def __call__(self, params):
+        "The `init` function"
+
+
+class TransformUpdateFn(Protocol):
+    def __call__(self, updates, state, params):
+        """The `update` function."""
+
+
+class RiemannianGradientTransformation(NamedTuple):
+    """Riemannian Gradient transformation."""
+
+    init: TransformInitFn
+    update: TransformUpdateFn
+
+
+class RiemannianEmptyState(NamedTuple):
+  """An empty state for all Riemannian gradient transformations."""
+
+  manifold_dict: FrozenDict[str, Any]
+
+
+
--- rieoptax/optimizers/transforms.py
+++ rieoptax/optimizers/transforms.py
@@ -19,16 +19,24 @@ class TraceState(NamedTuple):
 class EmptyState(NamedTuple):
   """An empty state for the simplest stateless transformations."""
 
-ScaleState = EmptyState
+
+class RiemannianEmptyState(NamedTuple):
+  """An empty state for all Riemannian gradient transformations."""
+  manifold_dict: FrozenDict[str, Any]
+
+  
+
+ScaleState = RiemannianEmptyState
 
 def scale(step_size) :
   """Scale updates by some fixed scalar `step_size`."""
 
-  def init_fn(params):
+  def init_fn(params, manifold_dict=None):
     del params 
+    manifold_dict = manifold_dict if manifold_dict else  
     return ScaleState()
 
-  def update_fn(updates, state, params=None, manifold_dict=None):
+  def update_fn(updates, state, params=None):
     del params
     del manifold_dict
     updates = step_size*updates.value
@@ -38,7 +46,7 @@ def scale(step_size) :
 
 
 def update_moment(updates, moments, decay):
-    """Compute the exponential moving average of the `order`-th moment."""
+    """Compute the exponential moving average of the first moment."""
     return jax.tree_util.tree_map(
         lambda g, t: (1 - decay) * g + decay * t, updates, moments
     )
@@ -47,7 +55,8 @@ def update_moment(updates, moments, decay):
 def update_moment_per_metric_norm(
     updates, moments, decay, manifold_dict, params
 ):
-    """Compute the exponential moving average of squared Riemannain metric norm."""
+    """Compute the exponential moving average of second moment of 
+       squared Riemannain metric norm."""
 
     def squared_metric_norm(g, m, p):
         """Calculates Riemmanian metric norm of tangent vector 'g' at base point 'p'."""
@@ -61,10 +70,21 @@ def update_moment_per_metric_norm(
         params,
     )
 
+@functools.partial(jax.jit, inline=True)
+def bias_correction(moment, decay, count):
+  """Performs bias correction. It becomes a no-op as count goes to infinity."""
+  bias_correction_ = 1 - decay**count
 
+  # Perform division in the original precision.
+  return jax.tree_util.tree_map(
+      lambda t: t / bias_correction_.astype(t.dtype), moment)
 
 def scale_by_radam(
-    b1: float = 0.9, b2: float = 0.99, eps: float = 1e-8, eps_root: float = 0.0
+    b1: float = 0.9,
+    b2: float = 0.99,
+    eps: float = 1e-8,
+    eps_root: float = 0.0,
+    ams_grad: bool = False,
 ) -> RiemannianGradientTransformation:
     """Rescale updates according to the Adam algorithm.
     References:
@@ -76,25 +96,30 @@ def scale_by_radam(
       eps: Term added to the denominator to improve numerical stability.
       eps_root: Term added to the denominator inside the square-root to improve
         numerical stability when backpropagating gradients through the rescaling.
+      ams_grad: Perform update according to ams_grad version of radam.
+        see [Sashank J. Reddi et al, 2019](https://arxiv.org/abs/1904.09237).
     Returns:
       A `RiemannianGradientTransformation` object.
     """
 
-    def init_fn(params):
+    def init_fn(params, ):
         mu = jax.tree_util.tree_map(  # First moment
             lambda t: jnp.zeros_like(t, dtype=mu_dtype), params
         )
         nu = jax.tree_util.tree_map(jnp.zeros_like, params)  # Second moment
         return ScaleByRadamState(count=jnp.zeros([], jnp.int32), mu=mu, nu=nu)
 
-    def update_fn(updates, state, params, manifold_dict):
+    def update_fn(updates, state, params):
         mu = update_moment(updates, state.mu, b1)
-        nu = update_moment_per_elem_norm(
-            updates, state.nu, b2, manifold_dict, params
-        )
+        nu = update_moment_per_elem_norm(updates, state.nu, b2, manifold_dict, params)
         count_inc = numerics.safe_int32_increment(state.count)
         mu_hat = bias_correction(mu, b1, count_inc)
         nu_hat = bias_correction(nu, b2, count_inc)
+        if ams_grad:
+
+
+        else:
+
         updates = jax.tree_util.tree_map(
             lambda m, v: m / (jnp.sqrt(v + eps_root) + eps), mu_hat, nu_hat
         )
