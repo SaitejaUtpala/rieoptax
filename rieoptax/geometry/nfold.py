6cb3e8f239914081e8b256ab69cc8dd33c73ff47
Saiteja Utpala <saitejautpala@gmail.com>
2023-02-21 09:24:47 +0530

    complete product manifold

--- rieoptax/geometry/nfold.py
+++ rieoptax/geometry/nfold.py
@@ -24,7 +24,74 @@ class NFoldManifold():
         """
         inp = vmap(self.manifold.inp, in_axes=(0,0,0))(bpt, tv_a, tv_b)
         return inp.sum()
+    
+    @partial(jit, static_argnums=(0,))
+    def exp(self, bpt: Array, tv: Array) -> Array:
+        """Riemannian Exponential map.
 
+        Args:
+            bpt: base_point.
+            tv: tangent_vec.
 
+        Returns:
+            returns Exp_{bpt}(tv).
+        """
+        exp = vmap(self.manifold.exp, in_axes=(0,0))(bpt, tv)
+        return exp
 
-        
+    @partial(jit, static_argnums=(0,))
+    def log(self, bpt: Array, pt: Array) -> Array:
+        """Riemannian Logarithm map.
+
+        Args:
+            bpt: base_point.
+            pt: tangent_vec.
+
+        Returns:
+            returns Log_{bpt}(pt).
+        """
+        log = vmap(self.manifold.log, in_axes=(0,0))(bpt, pt)
+        return log
+    
+    @partial(jit, static_argnums=(0,))
+    def egrad_to_rgrad(self, bpt: Array, egrad: Array) -> Array:
+        """Euclidean gradient to Riemannian Gradient Convertor.
+
+        Args:
+            bpt: base_point.
+            egrad: tangent_vec.
+
+        Returns:
+            returns Log_{bpt}(pt).
+        """
+        log = vmap(self.manifold.egrad_to_rgrad, in_axes=(0,0))(bpt, egrad)
+        return log
+
+    @partial(jit, static_argnums=(0,))
+    def dist(self, pt_a: Array, pt_b: Array) -> float:
+        """Distance between two points on the manifold induced by Riemannian metric.
+
+        Args:
+            pt_a: point on the manifold.
+            pt_b: point on the manifold.
+
+        Returns:
+            returns distance between pt_a, pt_b.
+        """
+        dist = vmap(self.manifold.dist, in_axes=(0,0))(pt_a, pt_a)
+        return dist.sum()
+
+    @partial(jit, static_argnums=(0,))
+    def ptrans(self, s_pt: Array, e_pt: Array, tv: Array) -> Array:
+        """Parallel Transport.
+
+        Args:
+            s_pt: start point.
+            e_pt: end point.
+            tv: tangent vector at start point.
+
+        Returns:
+            returns PT_{s_pt ->e_pt}(tv).
+        """
+        dist = vmap(self.manifold.dist, in_axes=(0,0,0))(s_pt, e_pt, tv)
+        return dist.sum()
