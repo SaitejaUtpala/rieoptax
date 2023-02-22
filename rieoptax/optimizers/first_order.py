40119346f8a7ca7b81983453a986fa540d6fc5a8
Saiteja Utpala <saitejautpala@gmail.com>
2023-02-22 22:02:28 +0530

    comment out radam for debugging

--- rieoptax/optimizers/first_order.py
+++ rieoptax/optimizers/first_order.py
@@ -2,26 +2,26 @@ from rieoptax.optimizers.base import RiemannianGradientTransformation
 
 from rieoptax.optimizers.combine import chain
 from rieoptax.optimizers.privacy import differentially_private_aggregate
-from rieoptax.optimizers.transforms import scale_by_learning_rate, scale_by_radam
+from rieoptax.optimizers.transforms import scale_by_learning_rate #, scale_by_radam
 
 
-def rsgd(learning_rate: float) -> RiemannianGradientTransformation:
-    """Riemannian (stochastic) gradient descent."""
-    return scale_by_learning_rate(learning_rate)
+# def rsgd(learning_rate: float) -> RiemannianGradientTransformation:
+#     """Riemannian (stochastic) gradient descent."""
+#     return scale_by_learning_rate(learning_rate)
 
-def radam(
-    learning_rate: float,
-    b1: float = 0.9,
-    b2: float = 0.999,
-    eps: float = 1e-8,
-    eps_root: float = 0.0,
-    ams_grad: bool = False,
-) -> RiemannianGradientTransformation:
-    """Riemannian adaptive moment estimation (ADAM)."""
-    return chain(
-        scale_by_radam(b1=b1, b2=b2, eps=eps, eps_root=eps_root, ams_grad=ams_grad),
-        scale_by_learning_rate(learning_rate),
-    )
+# def radam(
+#     learning_rate: float,
+#     b1: float = 0.9,
+#     b2: float = 0.999,
+#     eps: float = 1e-8,
+#     eps_root: float = 0.0,
+#     ams_grad: bool = False,
+# ) -> RiemannianGradientTransformation:
+#     """Riemannian adaptive moment estimation (ADAM)."""
+#     return chain(
+#         scale_by_radam(b1=b1, b2=b2, eps=eps, eps_root=eps_root, ams_grad=ams_grad),
+#         scale_by_learning_rate(learning_rate),
+#     )
 
 def dp_rsgd(
     learning_rate: float, norm_clip: float, sigma: float, seed: int
